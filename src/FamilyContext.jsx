bfa7d4b8543b70de90d37ea35c733834106d321a
CedricDC17 <cedric.dc91@gmail.com>
2025-04-20 00:39:07 +0200

    undo

--- src/FamilyContext.jsx
+++ src/FamilyContext.jsx
@@ -11,10 +11,7 @@ export function FamilyProvider({ children }) {
         // 1. Connexion anonyme
         signInAnonymously(auth)
         // 2. Mise à jour du user
-        const unsub = onAuthStateChanged(auth, u => {
-            console.log("User signed in:", u);
-            setUser(u);
-        });
+        const unsub = onAuthStateChanged(auth, u => setUser(u))
         return unsub
     }, [])
 
