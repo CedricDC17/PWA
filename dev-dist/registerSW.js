4c9553df96128340713bdeb0b6a738edd1ab9e22
CedricDC17 <cedric.dc91@gmail.com>
2025-04-20 11:54:17 +0200

    registerSW test pour chrome

--- dev-dist/registerSW.js
+++ dev-dist/registerSW.js
@@ -1,1 +1,16 @@
-if('serviceWorker' in navigator) navigator.serviceWorker.register('/dev-sw.js?dev-sw', { scope: '/', type: 'classic' })
+// main.js ou index.js
+import { registerSW } from 'virtual:pwa-register'
+
+const updateSW = registerSW({
+    onRegistered(r) {
+        // vérifie toutes les heures
+        setInterval(() => r && r.update(), 60 * 60 * 1000)
+    },
+    onNeedRefresh() {
+        // quand une nouvelle version est dispo, on la switche
+        updateSW()
+    }
+})
+
+
+if ('serviceWorker' in navigator) navigator.serviceWorker.register('/dev-sw.js?dev-sw', { scope: '/', type: 'classic' })
